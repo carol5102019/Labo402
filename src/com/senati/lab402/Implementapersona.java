3050666386f53176866ed8cfb230231b2ad08787
Alumno <Alumno@192.168.106.45>
2019-08-27 10:58:54 -0500

    Envio 01: Proyecto Persona

---
+++ src/com/senati/lab402/Implementapersona.java
@@ -0,0 +1,21 @@
+package com.senati.lab402;
+
+public class Implementapersona {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//CREA UN OBJETO POLICIA CON LA CLASE PERSONA
+	Persona policia = new Persona();
+	policia.id =1;
+	policia.nombre = "Roberto";
+	policia.apellido = "Gonzales";
+	policia.sexo = true;
+	policia.ecivil = 'S';
+	
+	
+	System.out.println(policia.toString());
+	
+
+	}
+
+}
